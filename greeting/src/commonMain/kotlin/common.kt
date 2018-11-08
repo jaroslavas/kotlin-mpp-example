dd28b4a426287924a0f009182ac8efda03caa83d
Jaroslav O <jaroslav.ozevic@nfq.lt>
2018-11-08 10:53:16 +0200

    Added suspend function

--- greeting/src/commonMain/kotlin/common.kt
+++ greeting/src/commonMain/kotlin/common.kt
@@ -1,9 +1,35 @@
 package org.greeting
 
+
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+
 expect class Platform() {
     val platform: String
 }
 
 class Greeting {
     fun greeting(): String = "Hello, ${Platform().platform}"
+
+
+    suspend fun anotherGreeting(): String {
+        return runBlocking {
+            delay(1500)
+            Platform().platform
+        }
+    }
+
+
+    fun anotherGreetingAsync(context: CoroutineContext): Deferred<String> {
+        return GlobalScope.async(context) {
+            anotherGreeting()
+        }
+    }
+
+}
+
+
+class DummyClass {
+    // Needed for native compiler to include CoroutineDispatcher interface in final framework
+    public val dummyDispatcher: CoroutineDispatcher? = null
 }
--- iosApp/iosApp/ViewController.swift
+++ iosApp/iosApp/ViewController.swift
@@ -22,5 +22,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBOutlet weak var label: UILabel!
+    
+    @IBAction func buttonPressed(){
+        
+        let greeting = Greeting()
+        let deferred = greeting.anotherGreetingAsync(context: UI())
+        deferred.invokeOnCompletion(handler: { _ in
+            print("deferred completed: \(String(describing: deferred.getCompleted()))")
+            self.label.text = "\(String(describing: deferred.getCompleted()))"            
+            return KotlinUnit()
+        })
+        deferred.invokeOnCompletion(onCancelling: true, invokeImmediately: true, handler: {error in
+            guard let error = error else {return KotlinUnit()}
+            print("deferred error: \(String(describing: error))")
+            self.label.text = "Error: \(String(describing: error))"
+            return KotlinUnit()
+        })
+        label.text = "Please wait..."
+    }
+    
 }
 
+public class UI: Kotlinx_coroutines_core_nativeCoroutineDispatcher {
+    override public func dispatch(context: KotlinCoroutineContext, block: Kotlinx_coroutines_core_nativeRunnable) {
+        DispatchQueue.main.async {
+            block.run()
+        }
+    }
+}
